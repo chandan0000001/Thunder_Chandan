476051bcc1e76662af1d3d9abb57681e71398f3d
chandan0000001 <kumardalaichandan803@gmail.com>
2026-06-27 23:00:07 +0530

    Writitng my own docs in medum

--- Javascript/Lecture07/structured.js
+++ Javascript/Lecture07/structured.js
@@ -1,44 +1,65 @@
-const Orginaluserx = {
-    name:"chandan",
-    age:32,
-    amount:2183,
-    state:"Hariyana",
-    vehicle: "Car",
-    adress : {
-        state: "Odisha",
-        blok: "huads"
-    },
-    number: [23,25,342,2334]
-}
+// const Orginaluserx = {
+//     name:"chandan",
+//     age:32,
+//     amount:2183,
+//     state:"Hariyana",
+//     vehicle: "Car",
+//     adress : {
+//         state: "Odisha",
+//         blok: "huads"
+//     },
+//     number: [23,25,342,2334]
+// }
 
-const deepclone = structuredClone(Orginaluserx)
-deepclone.name="Habiidulla";
-deepclone.age = 932;
-deepclone.adress.state= "Maharastra";
-deepclone.number.push(90);
+// const deepclone = structuredClone(Orginaluserx)
+// deepclone.name="Habiidulla";
+// deepclone.age = 932;
+// deepclone.adress.state= "Maharastra";
+// deepclone.number.push(90);
 
-console.log(Orginaluserx.name);
-console.log(Orginaluserx.amount);
-console.log(Orginaluserx.age);
-console.log(Orginaluserx.adress.state);
-console.log(Orginaluserx.number);
-
-
-
-console.log(deepclone.name);
-console.log(deepclone.amount);
-console.log(deepclone.age);
-console.log(deepclone.adress.state);
-console.log(deepclone.number);
+// console.log(Orginaluserx.name);
+// console.log(Orginaluserx.amount);
+// console.log(Orginaluserx.age);
+// console.log(Orginaluserx.adress.state);
+// console.log(Orginaluserx.number);
 
 
-// chandan
-// 2183
-// 32
-// Odisha
-// [ 23, 25, 342, 2334 ]
-// Habiidulla
-// 2183
-// 932
-// Maharastra
-// [ 23, 25, 342, 2334, 90 ]
+
+// console.log(deepclone.name);
+// console.log(deepclone.amount);
+// console.log(deepclone.age);
+// console.log(deepclone.adress.state);
+// console.log(deepclone.number);
+
+
+// // chandan
+// // 2183
+// // 32
+// // Odisha
+// // [ 23, 25, 342, 2334 ]
+// // Habiidulla
+// // 2183
+// // 932
+// // Maharastra
+// // [ 23, 25, 342, 2334, 90 ]
+
+
+// const user = {
+//     name: "Alice"
+//   };
+  
+//   // Let's create a "secret" ID for this user.
+//   const secretId = Symbol("userId");
+  
+//   // Add the property using the symbol as the key
+//   user[secretId] = "xyz-987-abc";
+  
+//   console.log(user); // { name: "Alice", [Symbol(userId)]: "xyz-987-abc" }
+
+
+
+
+  const user = {name:"Chandan", age: 43 }
+const secId = Symbol("userId")
+user[secId] = "233h2hv5234jb"
+console.log(user);
